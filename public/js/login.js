99d21daad390eaf21124d206e05edcaa7bcbf2d3
Josh Hawk <hawkjosh@icloud.com>
2022-08-02 13:48:22 -0400

    Created logic for login page

---
+++ public/js/login.js
@@ -0,0 +1,28 @@
+const loginPage = document.getElementById('login-page')
+
+const login = async(e) => {
+  e.preventDefault()
+
+  const username = document.getElementById('login-username').value.trim();
+
+  const password = document.getElementById('login-password').value.trim()
+
+  if(username && password) {
+    const response = await fetch('/api/users/login/', {
+      method: 'POST',
+      body: JSON.stringify({
+        username, password
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      }
+    })
+    if(response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+loginForm.addEventListener('submit', login);
