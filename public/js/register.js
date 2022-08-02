f77e67b5fe528b91ae31788432bf24670191a911
huntersteffner <huntersteffner@gmail.com>
2022-08-02 11:40:19 -0400

    Can register on frontend

---
+++ public/js/register.js
@@ -0,0 +1,31 @@
+const registerForm = document.getElementById('register-form')
+
+const register = async(e) => {
+    e.preventDefault()
+
+    const username = document.getElementById('register-username').value.trim()
+
+    const password = document.getElementById('register-password').value.trim()
+
+    const email = document.getElementById('register-email').value.trim()
+
+    if(username && password && email) {
+        const response = await fetch('/api/users/', {
+            method: 'POST',
+            body: JSON.stringify({
+                username,password,email
+            }),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+
+        if(response.ok) {
+            document.location.replace('/')
+        } else {
+            alert(response.statusText)
+        }
+    }
+}
+
+registerForm.addEventListener('submit', register)
