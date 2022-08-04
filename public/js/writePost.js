b1f12ef3030c335787216192d1a7b736d114cd24
Alkarias <alkarias29@gmail.com>
2022-08-04 00:13:36 -0400

    fixed categories

--- public/js/writePost.js
+++ public/js/writePost.js
@@ -1,4 +1,5 @@
 const submitButton = document.getElementById('submitButton');
+const dropdownOptions = document.getElementById('options');
 
 const createPost = async(event) => {
     event.preventDefault();
@@ -6,7 +7,7 @@ const createPost = async(event) => {
     // grabs all of the user input
     const title = document.getElementById('titleInput').value.trim();
     const content = document.getElementById('contentInput').value.trim();
-    const category = document.getElementById('dropdownMenuButton').value;
+    const category = document.getElementById('dropdownMenuButton').textContent.trim();
 
     if (!title) {
         // sets the border of the input to be red
@@ -22,23 +23,48 @@ const createPost = async(event) => {
         parentDiv.appendChild(small);
     }
 
-    // POST request that sends the information to the api
-    const response = await fetch('/api/posts/', {
-        method: 'POST',
-        body: JSON.stringify({
-            title, content, category
-        }),
-        headers: {
-            'Content-type': 'application/json'
-        }
-    })
-    if (response.ok) {
-        document.location.replace('/forum');
-    } else {
-        alert(response.statusText);
+    if (category === 'Categories') {
+        //sets the border of the category box to be red
+       document.getElementById('dropdownMenuButton')
+        .setAttribute('class','btn btn-light dropdown-toggle border-danger');
+        // grabs the parent element of the category box
+        const parentDiv = document.getElementById('dropdownMenuButton').parentElement;
+        // creates a new <small> element
+        const small = document.createElement('small');
+        // sets the classes and text of the <small> object appropriately
+        small.setAttribute('class', 'form-text text-danger');
+        small.textContent = 'You must pick the category';
+        // appends the new <small> element to the page
+        parentDiv.appendChild(small);
     }
 
+    // sends a POST request to append the table
+    if (title && category !== 'Categories') {
+        // POST request that sends the information to the api
+        const response = await fetch('/api/posts/', {
+            method: 'POST',
+            body: JSON.stringify({
+                title, content, category
+            }),
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        if (response.ok) {
+            document.location.replace('/forum');
+        } else {
+            alert(response.statusText);
+        }
+    }
 
 }   
+// sets the dropdown to the appropriate value of the button clicked
+const setCategory = async(event) => {
+    event.preventDefault();
+    
+    const categoriesButton = document.getElementById('dropdownMenuButton');
+    categoriesButton.textContent = event.target.textContent.trim();
+}
 
+dropdownOptions.addEventListener('click', setCategory);
 submitButton.addEventListener('click', createPost);
