545d077b7319a6a7eee30c3122715ef3ba1409e8
huntersteffner <huntersteffner@gmail.com>
2022-08-04 12:00:51 -0400

    Successfully wrote a comment

---
+++ public/js/post.js
@@ -0,0 +1,34 @@
+
+console.log('testing')
+
+const commentForm = document.getElementById('comment-form')
+const postId = commentForm.getAttribute('data-post')
+    console.log('postId: ' + postId)
+
+const addComment = async (e) => {
+    e.preventDefault()
+
+    const content = document.getElementById('comment-content').value
+    // const postId = window.location.toString().split("/")[
+    //   window.location.toString().split("/").length - 1
+    // ];
+    // console.log(postId)
+    
+
+
+    if(content) {
+        const response = await fetch('/api/replies/', {
+            method: 'POST',
+            body: JSON.stringify({content,postId}),
+            headers: {
+                'Content-type': 'application/json; charset=UTF-8',
+            }
+        })
+
+        if(response.ok) {
+            document.location.reload()
+        }
+    }
+}
+
+commentForm.addEventListener('submit', addComment)
