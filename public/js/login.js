df6b1d7e46ddb534c2aa70027ecb724cd6679dcf
Peru PC <paulocondori1@gmail.com>
2022-08-04 14:44:03 -0400

    login.js still doesnt work but its close

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,42 @@
 const loginForm = document.getElementById('login-form')
+const ResetUInfo  = document.getElementById('ResetUInfo')
 
+const nodemailer = require('nodemailer');
+require('dotenv').config()
+
+
+//NODEMAILER & FORGOT PASSWORD FUNCIONALITY
+const sendMail = () => {
+//step 1 
+let transporter = nodemailer.createTransport({
+  service: 'yahoo',
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD
+  }
+})
+
+//step 2
+let mailOptions = {
+  from: 'wookiesgold@yahoo.com',
+  to: 'paulocondori1@gmail.com',
+  subject: 'Nodemailer',
+  text: 'It works!'
+};
+
+//Step 3
+transporter.sendMail(mailOptions, function(err, data) {
+  if (err) {
+    console.log('Email was not sent') 
+  }else{
+    console.log('Email was Sent')
+  }
+})
+// Regular ^^^^^
+}
+  ResetUInfo.addEventListener('submit', sendMail);
+
+// LOGIN FUNCTIONALITY
 const login = async(e) => {
   e.preventDefault()
 
@@ -26,3 +63,6 @@ const login = async(e) => {
 }
 
 loginForm.addEventListener('submit', login);
+
+
+
