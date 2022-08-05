4384088bf4fad44d3db7ce731a678c8a53da2c66
Josh Hawk <hawkjosh@icloud.com>
2022-08-04 23:28:31 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,21 @@
-require('dotenv').config();
+const Sequelize = require("sequelize");
+require("dotenv").config();
 
-const Sequelize = require('sequelize');
+let sequelize;
 
 if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+	sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-    }
-  );
+	sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PASSWORD,
+		{
+			host: "localhost",
+			dialect: "mysql",
+			port: 3306,
+		}
+	);
 }
 
-
-// modules is in 22 intially
 module.exports = sequelize;
